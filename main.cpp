04b5482cd9eab35784b1195a98dc548b263470f9
Michel Calheiros <calheiros97@gmail.com>
2020-11-28 15:33:45 -0300

    added 1003 - Simple Sum

--- main.cpp
+++ main.cpp
@@ -1,27 +1,22 @@
 #include <iostream>
-#include <iomanip>
 
 using namespace std;
 
 int main() {
 
-    /**
-     * Escreva a sua solução aqui
-     * Code your solution here
-     * Escriba su solución aquí
-     */
+  /**
+   * Escreva a sua solução aqui
+   * Code your solution here
+   * Escriba su solución aquí
+   */
+  int A, B, SOMA;
 
-    double raio, area, pi;
-    
-    pi = 3.14159;
-    
-    cin >> raio;
-    
-    area = pi * (raio * raio);
-    
-    cout << fixed;
-    cout << setprecision(4);
-    cout << "A=" << area << endl;
+  cin >> A;
+  cin >> B;
 
-    return 0;
+  SOMA = A + B;
+
+  cout << "SOMA = " << SOMA << endl;
+
+  return 0;
 }
