563c2a0fd95c409243ad679459e49ce378190c82
Michel Calheiros <calheiros97@gmail.com>
2020-11-28 19:08:17 -0300

    added 1012 - Area

--- main.cpp
+++ main.cpp
@@ -1,22 +1,39 @@
 #include <iostream>
 #include <iomanip>
-#include <cmath>
+#include <sstream>
+#include <vector>
 
 using namespace std;
 
 int main() {
 
   const double PI = 3.14159;
-  double R, volume;
+  double i1, i2, i3;
+  double tri, cir, tra, squa, rect;
 
-  cin >> R;
+  string input;
+  getline(cin, input);
+
+  stringstream ss(input);
+
+  ss >> i1;
+  ss >> i2;
+  ss >> i3;
+
+  tri = (i1 * i3) / 2;
+  cir = (i3 * i3) * PI;
+  tra = ((i1 + i2) / 2) * i3;
+  squa = i2 * i2;
+  rect = i1 * i2;
 
   cout << fixed;
   cout << setprecision(3);
 
-  volume = (4.0/3.0) * PI * pow(R, 3);
-  
-  cout << "VOLUME = " << volume << endl;
+  cout << "TRIANGULO: " << tri << endl;
+  cout << "CIRCULO: " << cir << endl;
+  cout << "TRAPEZIO: " << tra << endl;
+  cout << "QUADRADO: " << squa << endl;
+  cout << "RETANGULO: " << rect << endl;
 
   return 0;
 }
