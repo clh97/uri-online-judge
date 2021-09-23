8b85538cfcd91d47ec82ee38c9db778a24f6616b
Michel Calheiros <michel@calheiros.dev>
2021-09-23 02:26:06 -0300

    WIP - added 1047 - Game Time with Minutes

---
+++ 1047/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <locale>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int initialHour, initialMinute, finalHour, finalMinute;
+  int resultHours, resultMinutes;
+
+  cin >> initialHour;
+  cin >> initialMinute;
+  cin >> finalHour;
+  cin >> finalMinute;
+
+  if (initialHour > finalHour) {
+    resultHours = 24 - (initialHour - finalHour);
+  } else if (finalHour > initialHour) {
+    resultHours = finalHour - initialHour;
+  } else if (initialHour == finalHour) {
+    resultHours = 24;
+  }
+
+  if ((finalMinute - initialMinute) < 0) {
+    resultMinutes = 60 + (finalMinute - initialMinute);
+    resultHours--;
+  } else if (finalMinute > initialMinute) {
+    resultMinutes = finalMinute - initialMinute;
+  }
+
+  if (initialHour == finalHour && initialMinute == finalMinute) {
+    resultHours = 24;
+    resultMinutes = 0;
+  }
+
+  cout << "O JOGO DUROU " << resultHours << " HORA(S) " << "E " << resultMinutes << " MINUTO(S)" << endl;
+
+  return 0;
+}
